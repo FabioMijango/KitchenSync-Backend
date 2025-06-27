8c77c57601b33693c6a7fc9328d2a4eb195840a0
Fabio Mijango <00156022@uca.edu.sv>
2025-06-26 20:42:17 -0600

    Add allowed origin for production in CORS configuration

--- BACKEND-KITCHEN/src/main/java/org/torres/backendkitchen/Config/CorsConfig.java
+++ BACKEND-KITCHEN/src/main/java/org/torres/backendkitchen/Config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://kitchen-sync-smoky.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
